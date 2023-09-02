b89b68defd5d1399e18c440e3382c50d1f0f6e24
Mark Powell <Mark.Powell@computer.org>
2023-09-01 17:52:12 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ZoomableImageView",
-    platforms: [.iOS(.v13), .tvOS(.v13), .visionOS(.v1)],
+    platforms: [.iOS(.v13), .tvOS(.v13)],
     products: [
         .library(
             name: "ZoomableImageView",
